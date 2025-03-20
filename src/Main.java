53e9fcdad249f72484306ad410777926b22018f1
D4nte-web <murugvo261998@gmail.com>
2025-03-20 23:43:36 +0300

    HW2

--- src/Main.java
+++ src/Main.java
@@ -17,34 +17,34 @@ public class Main {
             System.out.println(p);
         }
         System.out.println("Задача №5");
-        for (int e = 1904; e < 2096; e = e + 4){
+        for (int e = 1904; e <= 2096; e = e + 4){
             System.out.println(e + " год будет являться високостным.");
         }
         System.out.println("Задача №6");
-        for (int w = 7; w < 98; w = w + 7){
+        for (int w = 7; w <= 98; w = w + 7){
             System.out.println(w);
         }
         System.out.println("Задача №7");
-        for (int q = 1; q < 512; q = q * 2){
+        for (int q = 1; q <= 512; q = q * 2){
             System.out.println(q);
         }
         System.out.println("Задача №8");
         int month = 29000;
         int total = 0;
-        for (int y = 0; y < 12; y++){
+        for (int y = 0; y <= 12; y++){
             total = total + month;
             System.out.println("Месяц " + y + ", сумма накоплений равна " + total + " рублей.");
         }
         System.out.println("Задача №9");
         int solar = 29000;
         int savings = 0;
-        for (int a = 0; a < 12; a++){
+        for (int a = 0; a <= 12; a++){
             savings = savings + savings/100;
             savings = savings + solar;
             System.out.println("Месяц " + a + ", сумма накомплений равна " + savings + " рублей.");
         }
         System.out.println("Задача №10");
-        for (int f = 1; f < 11; f = f + 1){
+        for (int f = 1; f <= 10; f = f + 1){
             System.out.println("2" + "*" + f + "=" + f * 2);
         }
 
